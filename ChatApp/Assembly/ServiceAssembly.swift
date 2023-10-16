7b700d5cabb56822bdbe35c25a7fed7c15954169
Ivan Puzanov <ivanpuzan0v.dev@gmail.com>
2023-10-16 13:15:14 +0300

    Проведен рефакторинг кода

---
+++ ChatApp/Assembly/ServiceAssembly.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceAssembly.swift
+//  ChatApp
+//
+//  Created by Ivan Puzanov on 10.10.2023.
+//
+
+import Foundation
---
+++ ChatApp/Module/ChannelsList/Output/ChannelsListOutput.swift
@@ -0,0 +1,8 @@
+//
+//  ChannelsListOutput.swift
+//  ChatApp
+//
+//  Created by Ivan Puzanov on 10.10.2023.
+//
+
+import Foundation
